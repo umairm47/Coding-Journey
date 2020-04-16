92708908766d140ec34dd3e2a57bb1d2d9daa59d
umairm47 <48363297+umairm47@users.noreply.github.com>
2020-04-16 04:23:14 -0400

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var FirstArray = [12,14,15,16,17,18]
+console.log(FirstArray);
+console.log(FirstArray[0]);
+console.log(FirstArray.length);
+console.log(FirstArray.push('12'));
+console.log(FirstArray.pop());
+FirstArray.splice(4,0,20,22);
+
+console.log(FirstArray);
